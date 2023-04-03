d2fbe8f53161d25d4e013142a2b87d1ea291ff35
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-04-03 14:54:45 +0300

    create gh-pages

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 // just forget change settings in gitHub
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
